a967d96bb3428e410da55bca91d6a07c04459fa5
lpereiras <lucas.pdsantos11@gmail.com>
2021-11-17 14:53:00 -0300

    testes-e2e-com-cypress

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,11 +1,10 @@
 it('successfully logs in', () => {
-  const validEmailAddress = Cypress.env('USER_EMAIL')
-  const password = Cypress.env('USER_PASSWORD')
-
   cy.intercept('GET', '**/notes').as('getNotes')
 
-  cy.loginOnScratchApp(validEmailAddress, password)
-
+  cy.loginOnScratchApp(
+    Cypress.env('USER_EMAIL'),
+    Cypress.env('USER_PASSWORD'),
+    { cacheSession: false }
+  )
   cy.wait('@getNotes', { timeout: 8000 })
-  cy.contains('h1', 'Your Notes').should('be.visible')
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,8 @@
-Cypress.Commands.add('fillSignupFormAndSubmit', (emailAddress, password) => {
+Cypress.Commands.add('fillSignupFormAndSubmit', (
+  emailAddress,
+  password = Cypress.env('USER_PASSWORD')
+) => {
+
   cy.visit('/signup')
   cy.get('#email').type(emailAddress)
   cy.get('#password').type(password, { log: false })
@@ -7,9 +11,22 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (emailAddress, password) => {
   cy.get('#confirmationCode').should('be.visible')
 })
 
-Cypress.Commands.add('loginOnScratchApp', (validEmailAddress, password) => {
-  cy.visit('/login')
-  cy.get('#email').type(validEmailAddress, { log: false })
-  cy.get('#password').type(password, { log: false })
-  cy.contains('button', 'Login').click()
+Cypress.Commands.add('loginOnScratchApp', (
+  validEmailAddress = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD'),
+  { cacheSession = true } = {}
+) => {
+
+  const login = () => {
+    cy.visit('/login')
+    cy.get('#email').type(validEmailAddress, { log: false })
+    cy.get('#password').type(password, { log: false })
+    cy.contains('button', 'Login').click()
+    cy.contains('h1', 'Your Notes', { timeout: 6000 }).should('be.visible')
+  }
+  if (cacheSession) {
+    cy.session([validEmailAddress, password], login)
+  } else {
+    login()
+  }
 })
