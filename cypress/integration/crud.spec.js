f783b88aba8bb67d0582f9c6bbad9a450e786f6f
lpereiras <lucas.pdsantos11@gmail.com>
2021-11-17 17:12:54 -0300

    testes-e2e-com-cypress

--- cypress/integration/crud.spec.js
+++ cypress/integration/crud.spec.js
@@ -1,11 +1,14 @@
 it('CRUDs a note', () => {
-  // const faker = require('faker')
-  // const noteDescription = faker.lorem.words(4)
+  const faker = require('faker')
+  const noteDescription = faker.lorem.words(4)
 
   cy.intercept('GET', '**/notes').as('getNotes')
   cy.intercept('GET', '**/notes**').as('getNote')
   cy.loginOnScratchApp()
 
   cy.visit('/notes/new')
+  cy.get('#content').type(noteDescription)
+  cy.contains('button', 'Create').click()
 
+  cy.wait('@getNotes')
 })
